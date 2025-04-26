8c335d56afc5c3eae22f768a1fe475a726a87f69
Jabshshs <tintintotin73@gmail.com>
2025-04-27 00:05:02 +0800

    Update Jabscpm.py

--- Jabscpm.py
+++ Jabscpm.py
@@ -263,4 +263,15 @@ class Jabshshs:
         response = requests.post(f"{__ENDPOINT_URL__}/brake_car", params=params, data=payload)
         response_decoded = response.json()
         return response_decoded.get("ok")
+
+    def millage_car(self, car_id, custom):
+        payload = {
+        "account_auth": self.auth_token,
+        "car_id": car_id,
+        "custom": custom,
+        }
+        params = {"key": self.access_key}
+        response = requests.post(f"{__ENDPOINT_URL__}/millage_car", params=params, data=payload)
+        response_decoded = response.json()
+        return response_decoded.get("ok")
         
